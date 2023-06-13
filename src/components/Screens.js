aaf4755126cc7232f53b6bc96f58ac80aa5793f8
Balou9 <emmanuel.owosekun@gmail.com>
2023-06-13 19:15:37 +0200

    chore: add sound project description

--- src/components/Screens.js
+++ src/components/Screens.js
@@ -7,27 +7,66 @@ import guidomaria from '../pics/Guido_Maria.png'
 import heatrdy from '../pics/Heat_RDY.png'
 import nikedance from '../pics/Nike_Dance.png'
 
+const yearstr = "year:"
+const directedstr = "directed:"
+const musicstr = "music:"
+const brandstr = "brand:"
+
 export class Sound extends React.Component {
   render () {
     return (
       <div className="soundContainer">
         <div className="col1">
-        I offer MUSIC SUPERVISION and tailor-made compositions for commercials, television, and film productions. I am here to assist you with music production and sound consultancy. With my extensive portfolio and expertise, I help you find the perfect musical accompaniment for your commercial projects. Trust in my professional approach, and let us together create an unforgettable soundscape for your content. Contact me now and let's bring your vision to life!
+          I offer MUSIC SUPERVISION and tailor-made compositions for commercials, television, and film productions. I am here to assist you with music production and sound consultancy. With my extensive portfolio and expertise, I help you find the perfect musical accompaniment for your commercial projects. Trust in my professional approach, and let us together create an unforgettable soundscape for your content. Contact me now and let's bring your vision to life!
         </div>
         <div className="col2">
           <div className="picRow1">
-            <div><img className="soundPic" src={nikedance} alt="Nike_Dance"/></div>
-            <div><img className="soundPic" src={nikedance} alt="Nike_Dance"/></div>
-            <div><img className="soundPic" src={allforyou} alt="All_for_you"/></div>
+            <div>
+              <img className="soundPic" src={nikedance} alt="Nike_Dance"/>
+              <div>{yearstr}</div>
+              <div>{directedstr}</div>
+              <div>{musicstr}</div>
+              <div>{brandstr}</div>
+            </div>
+            <div>
+              <img className="soundPic" src={nikedance} alt="Nike_Dance"/>
+              <div>{yearstr}</div>
+              <div>{directedstr}</div>
+              <div>{musicstr}</div>
+              <div>{brandstr}</div>
+            </div>
+            <div>
+              <img className="soundPic" src={allforyou} alt="All_for_you"/>
+              <div>{yearstr}</div>
+              <div>{directedstr}</div>
+              <div>{musicstr}</div>
+              <div>{brandstr}</div>
+            </div>
           </div>
           <div className="picRow2">
-            <div><img className="soundPic" src={heatrdy} alt="Heat_RDY"/></div>
-            <div><img className="soundPic" src={guidomaria} alt="Guido_Maria"/></div>
-            <div><img  className="soundPic" src={danfox} alt="Dan_FOX"/></div>
+            <div>
+              <img className="soundPic" src={heatrdy} alt="Heat_RDY"/>
+              <div>{yearstr}</div>
+              <div>{directedstr}</div>
+              <div>{musicstr}</div>
+              <div>{brandstr}</div>
+            </div>
+            <div>
+              <img className="soundPic" src={guidomaria} alt="Guido_Maria"/>
+              <div>{yearstr}</div>
+              <div>{directedstr}</div>
+              <div>{musicstr}</div>
+              <div>{brandstr}</div>
+            </div>
+            <div>
+              <img  className="soundPic" src={danfox} alt="Dan_FOX"/>
+              <div>{yearstr}</div>
+              <div>{directedstr}</div>
+              <div>{musicstr}</div>
+              <div>{brandstr}</div>
+            </div>
           </div>
-
         </div>
-
       </div>
     )
   }
